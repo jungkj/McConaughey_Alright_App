001218d3710a2ddb82e7f2b9bcee0cbaad9dca11
Andy Jung <jungkj@bc.edu>
2023-02-20 17:34:47 -0500

    AlrightApp v1

--- McConaughey/ContentView.swift
+++ McConaughey/ContentView.swift
@@ -6,18 +6,65 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var stepperValue = 1
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            Text("How is Matt?")
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            
+            Image("matt")
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .shadow(radius: 30)
+            
+            Spacer()
+            
+            Stepper("How Alright is Matt?", value: $stepperValue, in: 1...5)
+                .font(.title2)
+            
+            Text("\(stepperValue)")
+                .font(.largeTitle)
+            
+            Button("Tell me!"){
+                playSound(soundName: "alright")
+            }
+            .buttonStyle(.bordered)
+
+            Spacer()
+            
         }
         .padding()
     }
+    func playSound(soundName: String){
+        guard let soundFile = NSDataAsset(name: soundName)else{
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        
+        
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.numberOfLoops = stepperValue - 1
+            audioPlayer.play()
+        }catch{
+            print("😡 ERROR: \(error.localizedDescription) creating audioPlayer")
+        }
+    }
 }
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
