713b559f75b3c3fd3c4aeae8ac989894ad64ed8a
Andy Jung <jungkj@bc.edu>
2023-02-20 16:44:43 -0500

    Initial Commit

---
+++ McConaughey/McConaugheyApp.swift
@@ -0,0 +1,17 @@
+//
+//  McConaugheyApp.swift
+//  McConaughey
+//
+//  Created by Andy Jung on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct McConaugheyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
